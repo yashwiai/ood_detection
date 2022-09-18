3125ef8f7c6ee3c3611609952e773fcbe1d2f6d9
Yash Khandelwal <yash@wadhwaniai.org>
2022-09-18 21:24:57 +0530

    Adding notebooks to create docs

--- ood_detection/_modidx.py
+++ ood_detection/_modidx.py
@@ -5,4 +5,11 @@ d = { 'settings': { 'branch': 'master',
                 'doc_host': 'https://yashwiai.github.io',
                 'git_url': 'https://github.com/yashwiai/ood_detection',
                 'lib_path': 'ood_detection'},
-  'syms': {'ood_detection.core': {'ood_detection.core.foo': ('core.html#foo', 'ood_detection/core.py')}}}
+  'syms': { 'ood_detection.core': { 'ood_detection.core.OODMetric': ('core.html#oodmetric', 'ood_detection/core.py'),
+                                    'ood_detection.core.OODMetric.__init__': ('core.html#oodmetric.__init__', 'ood_detection/core.py'),
+                                    'ood_detection.core.OODMetric.compute_rmd': ( 'core.html#oodmetric.compute_rmd',
+                                                                                  'ood_detection/core.py'),
+                                    'ood_detection.core.compute_mahalanobis_distance': ( 'core.html#compute_mahalanobis_distance',
+                                                                                         'ood_detection/core.py'),
+                                    'ood_detection.core.compute_mean_and_covariance': ( 'core.html#compute_mean_and_covariance',
+                                                                                        'ood_detection/core.py')}}}
